8bb7b1e20646f5adfd901158177fb984f3be207d
Blessing850 <amadi.b@northeastern.edu>
2026-01-13 17:26:16 -0800

    submission for lab1

---
+++ lab0/tempConverter.js
@@ -0,0 +1,9 @@
+// Average temperature in Houston, Texas in August (recent year)
+let fahrenheit = 85;
+
+let celsius = (fahrenheit - 32) * (5 / 9);
+let kelvin = celsius + 273.15;
+
+console.log("Temperature (Fahrenheit): " + fahrenheit);
+console.log("Temperature (Celsius): " + celsius);
+console.log("Temperature (Kelvin): " + kelvin);
