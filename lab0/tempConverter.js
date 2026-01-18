bb433ea68bd56f4591fe718e8cc316191bf054c9
Blessing850 <amadi.b@northeastern.edu>
2026-01-17 20:02:42 -0800

    lab2
    I tried moving the shapes

--- lab0/tempConverter.js
+++
@@ -1,9 +0,0 @@
-// Average temperature in Houston, Texas in August (recent year)
-let fahrenheit = 85;
-
-let celsius = (fahrenheit - 32) * (5 / 9);
-let kelvin = celsius + 273.15;
-
-console.log("Temperature (Fahrenheit): " + fahrenheit);
-console.log("Temperature (Celsius): " + celsius);
-console.log("Temperature (Kelvin): " + kelvin);
